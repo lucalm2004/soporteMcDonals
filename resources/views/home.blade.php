810c1eaec9ff9c433faa21c7a77b1ed38a631ab0
IvanMorenoRuiz <443948.joan23@fje.edu>
2024-03-11 17:00:35 +0100

    v12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     
-    <title>Document</title>
+    <title>Home Support</title>
 </head>
 <body>
     <main>
@@ -28,7 +28,7 @@
               <b></b>
               <a href="#">
                 <i class="fa fa-user nav-icon"></i>
-                <span class="nav-text">Profile</span>
+                <span class="nav-text">Incidencias</span>
               </a>
             </li>
   
@@ -64,47 +64,47 @@
         <section class="content">
           <div class="left-content">
             <div class="activities">
-              <h1>Popular Activities</h1>
+              <h1>Nuestras Sedes</h1>
               <div class="activity-container">
                 <div class="image-container img-one">
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/467cf682-03fb-4fae-b129-5d4f5db304dd" alt="tennis" />
+                  <img src="https://www.ejeprime.com/files//fotos/ciudades/22@-recurso-728.jpg" alt="tennis" />
                   <div class="overlay">
-                    <h3>Tennis</h3>
+                    <h3>Barcelona</h3>
                   </div>
                 </div>
   
                 <div class="image-container img-two">
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/3bab6a71-c842-4a50-9fed-b4ce650cb478" alt="hiking" />
+                  <img src="https://mitreworkspace.com/wp-content/uploads/2017/04/alquiler-oficinas-barcelona-sarria-santgervasi-workspace-business-center-centro-negocios-2-1.jpg.webp" alt="hiking" />
                   <div class="overlay">
-                    <h3>Hiking</h3>
+                    <h3>Oficinas Agbar</h3>
                   </div>
                 </div>
   
                 <div class="image-container img-three">
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c8e88356-8df5-4ac5-9e1f-5b9e99685021" alt="running" />
+                  <img src="https://www.roomdiseno.com/wp-content/uploads/2020/07/Cube_Berlin-3XN-roomdiseno16-750x500.jpg" alt="running" />
                   <div class="overlay">
-                    <h3>Running</h3>
+                    <h3>Berlin</h3>
                   </div>
                 </div>
   
                 <div class="image-container img-four">
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/69437d08-f203-4905-8cf5-05411cc28c19" alt="cycling" />
+                  <img src="https://images.adsttc.com/media/images/61d6/c116/3e4b/31d9/0100/0020/medium_jpg/JWA30.jpg?1641464075" alt="cycling" />
                   <div class="overlay">
-                    <h3>Cycling</h3>
+                    <h3>Oficinas Cube</h3>
                   </div>
                 </div>
   
                 <div class="image-container img-five">
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e1a66078-1927-4828-b793-15c403d06411" alt="yoga" />
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Edif%C3%ADcio_Montreal_01.JPG/640px-Edif%C3%ADcio_Montreal_01.JPG" alt="yoga" />
                   <div class="overlay">
-                    <h3>Yoga</h3>
+                    <h3>Montreal</h3>
                   </div>
                 </div>
   
                 <div class="image-container img-six">
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/7568e0ff-edb5-43dd-bff5-aed405fc32d9" alt="swimming" />
+                  <img src="https://metspace.com/wp-content/uploads/2023/10/6291ee6e64be79cce51b0d7f_montreal_cowork-header.png" alt="swimming" />
                   <div class="overlay">
-                    <h3>Swimming</h3>
+                    <h3>Oficina Montreal 21</h3>
                   </div>
                 </div>
               </div>
@@ -112,15 +112,15 @@
   
             <div class="left-bottom">
               <div class="weekly-schedule">
-                <h1>Weekly Schedule</h1>
+                <h1>Crear tickets</h1>
                 <div class="calendar">
                   <div class="day-and-activity activity-one">
                     <div class="day">
-                      <h1>13</h1>
-                      <p>mon</p>
+                      <h1>01</h1>
+                      <p>SW</p>
                     </div>
                     <div class="activity">
-                      <h2>Swimming</h2>
+                      <h2>Software</h2>
                       <div class="participants">
                         <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c61daa1c-5881-43f8-a50f-62be3d235daf" style="--i: 1" alt="" />
                         <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/90affa88-8da0-40c8-abe7-f77ea355a9de" style="--i: 2" alt="" />
@@ -128,73 +128,42 @@
                         <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e082b965-bb88-4192-bce6-0eb8b0bf8e68" style="--i: 4" alt="" />
                       </div>
                     </div>
-                    <button class="btn">Join</button>
+                    <button class="btn">Crear</button>
                   </div>
   
                   <div class="day-and-activity activity-two">
                     <div class="day">
-                      <h1>15</h1>
-                      <p>wed</p>
+                      <h1>02</h1>
+                      <p>HW</p>
                     </div>
                     <div class="activity">
-                      <h2>Yoga</h2>
+                      <h2>Hardware</h2>
                       <div class="participants">
                         <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c61daa1c-5881-43f8-a50f-62be3d235daf" style="--i: 1" alt="" />
                         <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/32037044-f076-433a-8a6e-9b80842f29c9" style="--i: 2" alt="" />
                       </div>
                     </div>
-                    <button class="btn">Join</button>
-                  </div>
-  
-                  <div class="day-and-activity activity-three">
-                    <div class="day">
-                      <h1>17</h1>
-                      <p>fri</p>
-                    </div>
-                    <div class="activity">
-                      <h2>Tennis</h2>
-                      <div class="participants">
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/32037044-f076-433a-8a6e-9b80842f29c9" style="--i: 1" alt="" />
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e082b965-bb88-4192-bce6-0eb8b0bf8e68" style="--i: 2" alt="" />
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c61daa1c-5881-43f8-a50f-62be3d235daf" style="--i: 3" alt="" />
-                      </div>
-                    </div>
-                    <button class="btn">Join</button>
-                  </div>
-  
-                  <div class="day-and-activity activity-four">
-                    <div class="day">
-                      <h1>18</h1>
-                      <p>sat</p>
-                    </div>
-                    <div class="activity">
-                      <h2>Hiking</h2>
-                      <div class="participants">
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/07d4fa6f-6559-4874-b912-3968fdfe4e5e" style="--i: 1" alt="" />
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/32037044-f076-433a-8a6e-9b80842f29c9" style="--i: 2" alt="" />
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/07d4fa6f-6559-4874-b912-3968fdfe4e5e" alt="" />
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c61daa1c-5881-43f8-a50f-62be3d235daf" style="--i: 4" alt="" />
-                        <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/90affa88-8da0-40c8-abe7-f77ea355a9de" style="--i: 5" alt="" />
-                      </div>
-                    </div>
-                    <button class="btn">Join</button>
+                    <button class="btn">Crear</button>
                   </div>
                 </div>
               </div>
   
+
+
+
               <div class="personal-bests">
-                <h1>Personal Bests</h1>
+                <h1>Incidencias Comunes</h1>
                 <div class="personal-bests-container">
                   <div class="best-item box-one">
-                    <p>Fastest 5K Run: 22min</p>
-                    <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/242bbd8c-aaf8-4aee-a3e4-e0df62d1ab27" alt="" />
+                    <p>Problemas con el teclado</p>
+                    <img src="{{ asset('img/teclado.png') }}" alt="" />
                   </div>
                   <div class="best-item box-two">
-                    <p>Longest Distance Cycling: 4 miles</p>
+                    <p>Conexion remota fallida</p>
                     <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/a3b3cb3a-5127-498b-91cc-a1d39499164a" alt="" />
                   </div>
                   <div class="best-item box-three">
-                    <p>Longest Roller-Skating: 2 hours</p>
+                    <p>Proyector sin señal</p>
                     <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e0ee8ffb-faa8-462a-b44d-0a18c1d9604c" alt="" />
                   </div>
                 </div>
@@ -212,50 +181,34 @@
               <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/40b7cce2-c289-4954-9be0-938479832a9c" alt="user" />
             </div>
   
-            <div class="active-calories">
-              <h1 style="align-self: flex-start">Active Calories</h1>
+            {{-- <div class="active-calories">
+              <h1 style="align-self: flex-start">Mis tickets</h1>
               <div class="active-calories-container">
-                <div class="box" style="--i: 85%">
+                <div class="box" style="--i: 20%">
                   <div class="circle">
-                    <h2>85<small>%</small></h2>
+                    <h2>20<small>%</small></h2>
                   </div>
                 </div>
                 <div class="calories-content">
-                  <p><span>Today:</span> 400</p>
-                  <p><span>This Week:</span> 3500</p>
-                  <p><span>This Month:</span> 14000</p>
+                  <p><span>Hoy:</span> 400</p>
+                  <p><span>Esta Semana:</span> 3500</p>
+                  <p><span>Este Mes:</span> 14000</p>
                 </div>
               </div>
-            </div>
+            </div> --}}
   
-            <div class="mobile-personal-bests">
-              <h1>Personal Bests</h1>
-              <div class="personal-bests-container">
-                <div class="best-item box-one">
-                  <p>Fastest 5K Run: 22min</p>
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/05dfc444-9ed3-44cc-96af-a9cf195f5820" alt="" />
-                </div>
-                <div class="best-item box-two">
-                  <p>Longest Distance Cycling: 4 miles</p>
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/9ca170e9-1252-4fa6-8677-36493540c1f2" alt="" />
-                </div>
-                <div class="best-item box-three">
-                  <p>Longest Roller-Skating: 2 hours</p>
-                  <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/262d1611-ed4c-4297-981c-480cf7f95714" alt="" />
-                </div>
-              </div>
-            </div>
+ 
   
             <div class="friends-activity">
-              <h1>Friends Activity</h1>
+              <h1>Valoraciones</h1>
               <div class="card-container">
                 <div class="card">
                   <div class="card-user-info">
                     <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/9290037d-a5b2-4f50-aea3-9f3f2b53b441" alt="" />
                     <h2>Jane</h2>
                   </div>
-                  <img class="card-img" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/bef54506-ea45-4e42-a1b6-23a48f61c5e8" alt="" />
-                  <p>We completed the 30-Day Running Streak Challenge!🏃‍♀️🎉</p>
+                  <img class="card-img" src="https://media.licdn.com/dms/image/C4E0DAQHtvwXaDzQZzA/learning-public-crop_288_512/0/1568667753141?e=2147483647&v=beta&t=5D1TuC49hG8L86aLexLgxT8vZK_37h1ZgRRRyCgTbgo" alt="" />
+                  <p>"Acceso remoto solucionado rapidamente. Gracias por vuestra ayuda tan útil y confiable."🎉</p>
                 </div>
   
                 <div class="card card-two">
@@ -263,8 +216,8 @@
                     <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/42616ef2-ba96-49c7-80ea-c3cf1e2ecc89" alt="" />
                     <h2>Mike</h2>
                   </div>
-                  <img class="card-img" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/2dcc1b94-06c5-4c62-b886-53b9e433fd44" alt="" />
-                  <p>I just set a new record in cycling: 30 miles!💪</p>
+                  <img class="card-img" src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/03/oficina-casa-2251675.jpg" alt="" />
+                  <p>"La reparación del monitor fue rápida."💪</p>
                 </div>
               </div>
             </div>
